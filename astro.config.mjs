fe639d9dd10e0f8f36f7bd4650009c88bf32272c
Rob Martin <Rob.martin.5758@gmail.com>
2026-09-25 19:19:31 -0400

    fixnetlify edge function not used

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import tina from '@tinacms/astro/integration';
 import { tinaAdminDevRedirect } from '@tinacms/astro/vite';
 
 const siteUrl =
-  process.env.SITE_URL ||
-  process.env.URL ||
-  'http://localhost:4321';
+  process.env.SITE_URL || process.env.URL || 'http://localhost:4321';
 
 export default defineConfig({
   site: siteUrl,
   output: 'static',
-  adapter: netlify(),
+  adapter: netlify({
+    devFeatures: {
+      edgeFunctions: false,
+    },
+  }),
   trailingSlash: 'never',
   integrations: [tina()],
   vite: {
