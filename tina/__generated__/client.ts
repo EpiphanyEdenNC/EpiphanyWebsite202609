e923e4ffd00f80399ee42934e778cf665947012f
Rob Martin <Rob.martin.5758@gmail.com>
2026-09-24 21:21:55 -0400

    remove the tina generated file not needed

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ url: "http://localhost:4001/graphql", token: "15891cbea3de5596b9b528ac7c6a1fe87fe506e0", queries,  });
-export default client;
-  
